6929bae4a65f1e3e62a1f00dcdc02049429c997c
Tianwaibian <33583551+Tianwaibian@users.noreply.github.com>
2017-12-06 23:37:30 +0200

    e

--- data/create_human.R
+++ data/create_human.R
@@ -18,28 +18,58 @@ dim(gii)
 summary(gii)
 
 #  rename the variables 
-colnames(hd)[3] <- 'HDI'
-colnames(hd)[4] <- 'Exp.lif'
-colnames(hd)[5] <- 'Exp.edu'
-colnames(hd)[6] <- 'Mean.edu'
-colnames(hd)[7] <- 'GNI.per'
-colnames(hd)[8] <- 'Rank(GNI-HDI)'
-colnames(gii)[3] <- 'gii'
-colnames(gii)[4] <- 'MMR'
-colnames(gii)[5] <- 'ABR'
-colnames(gii)[6] <- 'Percent.Rpp'
-colnames(gii)[7] <- 'edu2F'
-colnames(gii)[8] <- 'edu2M'
-colnames(gii)[9] <- 'LabF'
-colnames(gii)[10] <- 'LabM'
+
+colnames(hd)
+a <- c('HDI.Rank', 'Country', 'HDI', 'Life.Exp', 'Edu.Exp', 'Edu.mean', 'GNI', 'Rank(GNI-HDI)')
+colnames(hd) <- a
+colnames(gii)
+b <- c('GII.Rank', 'Country', 'gii', 'Mat.Mor', 'Ado.Birth', 'Parli.F', 'Edu2.F', 'Edu2.M', 'Lab.F', 'Lab.M')
+colnames(gii) <- b
 
 # Mutate the ¡°Gender inequality¡± data and create two new variables. 
 library(dplyr)
-Gii <- mutate(gii, Rfm_edu = edu2F / edu2M, Rfm_lab = LabF / LabM)
+Gii <- mutate(gii, Edu2.FM = Edu2.F / Edu2.M, Lab.FM = Lab.F / Lab.M)
 
 # Join together the two datasets using the variable Country as the identifier
 human <- inner_join(hd, Gii, by = "Country")
+# 195 observations of 19 variables
 # save the joined data "human" in data folder
 setwd('D:/github/IODS-project/data')
 write.table(human, file = 'human.txt', row.names = F, sep = ',')
-
+glimpse(human)
+# access the stringr package
+install.packages('tidyr')
+library(stringr)
+# look at the structure of the GNI column in 'human'
+str(human$GNI)
+# remove the commas from GNI and print out a numeric version of it
+str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric
+# columns to keep
+names(human)
+keep <- c( "Country", "Edu2.FM", "Lab.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+# select the 'keep' columns
+human <- select(human, one_of(keep))
+# print out a completeness indicator of the 'human' data
+complete.cases(human)
+# print out the data along with a completeness indicator as the last column
+data.frame(human[-1], comp = complete.cases(human))
+# Remove all rows with missing values
+human_ <- filter(human, complete.cases(human))
+human_
+# Remove the observations which relate to regions instead of countries
+# look at the last 10 observations of human
+tail(human_, n = 10)
+# the last 7 row should be removed.
+# define the last indice we want to keep
+last <- nrow(human_) - 7
+# choose everything until the last 7 observations
+Human <- human_[1: last, ]
+# add countries as rownames
+rownames(Human) <- Human$Country
+# remove the country name column from the data
+names(Human)
+Human <- Human[, c(2 : 9)]
+# The data should now have 155 observations and 8 variables.
+# Save the human data in your data folder including the row names
+getwd()
+write.table(Human, file = 'human.txt', row.names = T, sep = ',')
